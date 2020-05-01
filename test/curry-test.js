292ee49ed60832f48ef2ee1a30fb3d22b9f5220f
Rory Hodgson <29726065+rorybot@users.noreply.github.com>
2020-04-30 21:21:55 -0700

    Passes test for chunker

--- test/curry-test.js
+++ test/curry-test.js
@@ -1,15 +1,14 @@
 var assert = require('assert');
 const pipeline = require('../pipeline');
+const curryable = require("../curryable-functions");
 const arr1 = [...Array(100)].map(() => {
   return Math.floor(Math.random() * 9);
 });
 
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal([1, 2, 3].indexOf(4), -1);
-    });
-  });
-});
-
-console.log(pipeline.findHighestNumberInInterval(arr1, 4,5));
+describe('Pipeline components', ()=>
+  describe('arrayChunker', ()=>
+    it('number of arrays should equal rounded up length of origianl divided by the interval', () =>
+      assert.equal(curryable.arrayChunker(1)(arr1).length, 100 )
+    )
+  )
+)
