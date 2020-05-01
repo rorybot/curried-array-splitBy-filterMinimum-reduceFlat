874316c87014fad95784bfa7fedba1769b10b4eb
Rory Hodgson <29726065+rorybot@users.noreply.github.com>
2020-04-30 19:59:43 -0700

    clean up

--- curry.js
+++ curry.js
@@ -1,7 +1,7 @@
 const arr1 = [...Array(100)].map(() => {
   return Math.floor(Math.random() * 9);
 });
-console.log("Oriignal array:", arr1);
+
 
 const arrayChunker2 = amount => arrayToChunk =>
   arrayToChunk
@@ -13,22 +13,22 @@ const arrayChunker2 = amount => arrayToChunk =>
     )
     .filter(el => el);
 const chunkedArray = arrayChunker2(10)(arr1);
-console.log("Chunked array", chunkedArray);
+
 
 const highestVals = limit => array =>
   array.map(arr => arr.filter(el => el >= limit)).flat();
 const highestValsActual = highestVals(4)(chunkedArray);
 
-console.log("Highest vals array flattened", highestValsActual);
+
 
 const reducer = array => array.reduce((el1, el2) => el1 + el2);
-console.log(reducer(highestValsActual));
 
-// const reductionPipeline = (chunkThatArray) => x => (limitAndFlattenThoseArrays(x)) => reduceThemDown(z)
 const pipe = (...functions) => args =>
   functions.reduce((arg, fn) => fn(arg), args);
 
-const pipedResult = pipe(arrayChunker2(10), highestVals(4), reducer)(arr1);
-console.log("composed!", pipedResult);
+exports.finalResult = () => {
+  const pipedResult = pipe(arrayChunker2(10), highestVals(4), reducer)(arr1);
 
-// console.log(reductionPipeline(arrayChunker2)(highestVals(3))(reducer))
+  return pipedResult;
+
+}
