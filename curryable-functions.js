39346dd89679b9f84bfe8226d9e207c0a28c39f0
Rory Hodgson <29726065+rorybot@users.noreply.github.com>
2020-04-30 20:47:59 -0700

    Exported curried functions

---
+++ curryable-functions.js
@@ -0,0 +1,17 @@
+
+
+exports.arrayChunker = amount => arrayToChunk =>
+  arrayToChunk
+    .map(
+      (num, i) =>
+        (i + amount) % amount == 0 && amount >= 3
+          ? arrayToChunk.slice(i, i + amount)
+          : false
+    )
+    .filter(el => el);
+
+
+exports.highestVals = limit => array =>
+  array.map(arr => arr.filter(el => el >= limit)).flat();
+
+exports.reducer = array => array.reduce((el1, el2) => el1 + el2);
