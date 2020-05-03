ca52b1382b6a03da41f42b773200cbcdd27b0d2d
Rory Hodgson <29726065+rorybot@users.noreply.github.com>
2020-05-02 17:33:42 -0700

    Added different, stacked-argument format of pipeing functions

--- pipeline.js
+++ pipeline.js
@@ -4,10 +4,14 @@ const pipe = (...functions) => args =>
   functions.reduce((arg, fn) => fn(arg), args);
 
 exports.findHighestNumberInInterval = (array, interval, threshold) => {
-  const pipedResult = pipe(
+  return pipe(
     curryable.arrayChunker(interval),
     curryable.highestVals(threshold),
     curryable.reducer
   )(array);
-  return pipedResult;
 };
+
+exports.differentPipe = (array, interval, threshold) =>
+  curryable.reducer(
+    curryable.highestVals(threshold)(curryable.arrayChunker(interval)(array))
+  );
